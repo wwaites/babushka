b93b9448515f2983bfb2b5d60591bd9ae2cfcb39
William Waites <ww@groovy.net>
2017-11-01 17:33:39 +0000

    optionally access point or station

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -10,16 +10,21 @@ void setup_wifi() {
   char host[16];
   sysname = host;
 
+#if WIFI_APMODE
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(WIFI_SSID, WIFI_PSK);
+#else
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PSK);
-
   WiFi.macAddress(mac);
-  snprintf(host, sizeof(host), "%s-%02x%02%02x", HOSTNAME, mac[4], mac[5], mac[6]);
 
+  snprintf(host, sizeof(host), "%s-%02x%02%02x", HOSTNAME, mac[4], mac[5], mac[6]);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     delay(5000);
     ESP.restart();
   }
+#endif
+
 
 //  pointless because no logging yet
 //  IPAddress addr = WiFi.localIP();
